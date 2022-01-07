8c134c4bf8439ffe3c35683fc23ba6709c23dd2d
Jayce Mitchell <chimerror@live.com>
2022-01-06 17:33:21 -0800

    Initial Mesomorph Behaviour Implementation

---
+++ galvantula/Assets/MesomorphBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class MesomorphBehaviour : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float Cowiness = 0.0f;
+
+    private float CurrentCowiness
+    {
+        get
+        {
+            return Mathf.Clamp01(_animator.GetFloat("Cowiness"));
+        }
+
+        set
+        {
+            var clampedValue = Mathf.Clamp01(value);
+            _animator.SetFloat("Cowiness", clampedValue);
+            Cowiness = clampedValue;
+        }
+    }
+
+    private Animator _animator;
+    private SkinnedMeshRenderer _meshRenderer;
+    private Material _cowMaterial;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        _cowMaterial = _meshRenderer.materials[1];
+        CurrentCowiness = Cowiness;
+
+    }
+
+    private void Update()
+    {
+        if (CurrentCowiness != Cowiness)
+        {
+            CurrentCowiness = Cowiness;
+        }
+
+        var currentCowColor = _cowMaterial.color;
+        currentCowColor.a = Mathf.Clamp01(CurrentCowiness);
+        _cowMaterial.color = currentCowColor;
+    }
+
+}
